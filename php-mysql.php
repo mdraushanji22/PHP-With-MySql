73fcbeea5a5a56d00a9c66f2221fce8df8f2bed4
mdraushan <mdraushanji22@gmail.com>
2026-01-20 19:22:38 +0530

    php-mysql

---
+++ php-mysql.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = null;
+$database = "college";
+
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->$connect_error) {
+}

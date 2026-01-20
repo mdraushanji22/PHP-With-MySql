3aef8ae2e82f9645957c99e939139acd3d098db5
mdraushan <mdraushanji22@gmail.com>
2026-01-20 23:29:32 +0530

    php

--- php-mysql.php
+++ php-mysql.php
@@ -5,5 +5,13 @@ $password = null;
 $database = "college";
 
 $conn = new mysqli($host, $username, $password, $database);
-if ($conn->$connect_error) {
+
+// Correct error check
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connection successful";
+echo "<br>";
+$result = $conn->query("show tables")->fetch_all();
+print_r($result);

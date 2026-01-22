0e917e1ee76180ca89a74e1fef52b362dff1d6d0
mdraushan <mdraushanji22@gmail.com>
2026-01-22 11:43:00 +0530

    insert-data

---
+++ Display-data.php/insert.php
@@ -0,0 +1,33 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "college";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+if (isset($_POST['name'])) {
+    $name  = $_POST['name'];
+    $course  = $_POST['course'];
+    $batch  = $_POST['batch'];
+    $city  = $_POST['city'];
+    $year  = $_POST['year'];
+}
+
+// Insert Query
+$sql = "INSERT INTO students (name, course, batch,city,year) VALUES ('$name', '$course', '$batch', '$city','$year')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record inserted successfully";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
